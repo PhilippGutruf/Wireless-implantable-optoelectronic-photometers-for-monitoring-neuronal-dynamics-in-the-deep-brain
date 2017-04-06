611927b3d40789bd6f591e8075db98e09b412c62
PhilippGutruf <PhilippGutruf@googlemail.com>
2017-04-06 12:20:13 -0500

    Add files via upload

---
+++ recieve_side_ATmega328P.ino
@@ -0,0 +1,47 @@
+/* Philipp Gutruf
+ * wireless photometry
+ * written for ATmega328P
+ * handles data aquisition and wireless dataconnection via IR LED
+ * dataconnection is handled via NEC protocol
+ */
+#include <IRremote.h>
+
+int RECV_PIN = 5; 
+
+IRrecv irrecv(RECV_PIN);
+
+decode_results results;
+
+bool code=false;
+
+void setup()
+{
+  Serial.begin(9600);
+  irrecv.enableIRIn();
+}
+
+void dump(decode_results *results) 
+{
+  
+  if (results->decode_type == NEC) 
+  {
+    code=true;
+  }
+  
+}
+
+void loop() 
+{
+  if (irrecv.decode(&results)) 
+  {  
+    dump(&results);
+    if(code==true)
+    {
+      Serial.print(millis());
+      Serial.print(",");
+      Serial.println(results.value);
+      code=false;
+    }
+    irrecv.resume();
+  }
+}
